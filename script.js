ab803ac1e8935209d5830292a8966414d42e44f0
CASTICRI003 <ccastillo0924@gmail.com>
2022-11-14 21:56:55 -0600

    added button functionality and the init function

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ var h3 = document.getElementById('03');
 var h4 = document.getElementById('04');
 var h5 = document.getElementById('05');
 
+var Input;
+//function that runs when page is loaded
+function init() {
+  Input = json.parse(localStorage.getItem('plannerInput'));
+  if (Input === 0) {
+    return;
+  } else {
+    renderSchedule();
+  }
+};
+//function that runs when button is clicked
+$('.saveBtn').on('click', function (event) {
+  event.preventDefault();
+
+  var plannerInput = {
+     h9: h9.value,
+     h10: h10.value,
+     h11: h11.value,
+     h12: h12.value,
+     h1: h1.value,
+     h2: h2.value,
+     h3: h3.value,
+     h4: h4.value,
+     h5: h5.value
+  };
+
+  localStorage.setItem('plannerInput', json.stringify(plannerInput));
+
+});
+
+
+function renderSchedule() {
+  Input = json.parse(localStorage.getItem('plannerInput'));
+
+  
+}
+
+init();
